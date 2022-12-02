7cca58ac5b320f48055c3e511e3ee9ef034ff300
Alex <alex.parra.purroy@gmail.com>
2022-12-02 18:23:10 +0100

    Agregar cache y funciones utilizadas

--- sistemas.c
+++ sistemas.c
@@ -11,3 +11,18 @@
 #define DIREC_ETQ 5
 #define TAM_RAM 4096
 
+//Estructura de la cache
+typedef struct {
+        unsigned char ETQ;
+        unsigned char Data[TAM_LINEA];
+}T_CACHE_LINE;
+
+//Funciones utilizadas
+void hexadecimal_binario(int direccion, int *bin);
+void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
+int binario_decimal(int *binario, int num);
+void positivo_cache(int time, int direccion, int etiqueta, int linea, int palabra, T_CACHE_LINE *tbl, char *frases, int *alfa);
+void VolcarCACHE(T_CACHE_LINE *tbl); 
+void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque); 
+void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]); 
+
