656a226af9c4174e93ee0c9dc93dbc8286dc4cc2
OscarMaestre <oscar.maestre@live.u-tad.com>
2022-12-02 17:42:48 +0100

    intro 2

--- sistemas.c
+++ sistemas.c
@@ -1,3 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+
+#define TAM_BUS 12
+#define NUM_FILAS 8
+#define TAM_LINEA 16
+#define DIREC_PALABRA 4
+#define DIREC_LINEA 3
+#define DIREC_ETQ 5
+#define TAM_RAM 4096
