1150894b601fd9954bfa58de80c304ad03df9281
Alex <alex.parra.purroy@gmail.com>
2022-12-02 18:20:49 +0100

    Agregar cache y funciones utilizadas

--- sistemas.c
+++ sistemas.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
 #define TAM_BUS 12
 #define NUM_FILAS 8
@@ -9,3 +10,4 @@
 #define DIREC_LINEA 3
 #define DIREC_ETQ 5
 #define TAM_RAM 4096
+
